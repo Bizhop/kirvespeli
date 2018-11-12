8bc1f7125533704e4e4616156ca3421cb7b35089
Ville Piispa <ville.piispa@gmail.com>
2018-11-12 16:26:27 +0200

    test for anything goes probability

--- src/test/java/fi/bizhop/jassu/util/LotteryTest.java
+++ src/test/java/fi/bizhop/jassu/util/LotteryTest.java
@@ -8,17 +8,20 @@ public class LotteryTest {
 
     @Test
     public void doubleOrNothing() {
+        final int ROUNDS = 10000;
         double bank = 1000;
         double wager = 10;
+        double winnings = 0;
 
         double bankMin = 1000;
         double bankMax = 1000;
 
         Lottery lot = new Lottery(98);
 
-        for(int i=0; i<10000000; i++) {
-            bank -= wager;
-            bank += lot.doubleOrNothing(wager);
+        for(int i=0; i<ROUNDS; i++) {
+            double winning = lot.doubleOrNothing(wager);
+            winnings += winning;
+            bank += winning - wager;
             if(bankMin > bank) {
                 bankMin = bank;
             }
@@ -28,5 +31,34 @@ public class LotteryTest {
         }
 
         System.out.println(String.format("Bank: %f, (%f - %f)", bank, bankMin, bankMax));
+        System.out.println(String.format("Wagered: %f, winnings: %f, percentage: %f", ROUNDS * wager, winnings, winnings/(ROUNDS * wager)));
+    }
+
+    @Test
+    public void anythingGoes() {
+        final int ROUNDS = 10000;
+        double bank = 1000;
+        double wager = 10;
+        double winnings = 0;
+
+        double bankMin = 1000;
+        double bankMax = 1000;
+
+        Lottery lot = new Lottery(98);
+
+        for(int i=0; i<ROUNDS; i++) {
+            double winning = lot.anythingGoes(wager, 90);
+            winnings += winning;
+            bank += winning - wager;
+            if(bankMin > bank) {
+                bankMin = bank;
+            }
+            if(bankMax < bank) {
+                bankMax = bank;
+            }
+        }
+
+        System.out.println(String.format("Bank: %f, (%f - %f)", bank, bankMin, bankMax));
+        System.out.println(String.format("Wagered: %f, winnings: %f, percentage: %f", ROUNDS * wager, winnings, winnings/(ROUNDS * wager)));
     }
 }
