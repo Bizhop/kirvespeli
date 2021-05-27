4237df3115407622a9d0f884a0f17afd1f5326c9
Ville Piispa <ville.piispa@gmail.com>
2021-05-27 13:04:31 +0300

    update io.jsonwebtoken library from 0.9.1 -> 0.11.2 (#46)

--- src/main/java/fi/bizhop/jassu/security/JWTAuth.java
+++ src/main/java/fi/bizhop/jassu/security/JWTAuth.java
@@ -4,22 +4,26 @@ import fi.bizhop.jassu.Application;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.apache.logging.log4j.*;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class JWTAuth {
     private static final Logger LOG = LogManager.getLogger(JWTAuth.class);
     private static final long EXPIRATION_TIME = 14 * 24 * 60 * 60 * 1000; //14 days
-    private static final String JWT_SECRET;
+    private static final SecretKey JWT_KEY;
     public static final String JWT_TOKEN_PREFIX = "JWT ";
 
     static {
-        JWT_SECRET = System.getenv("JASSU_JWT_SECRET");
-        if(JWT_SECRET == null) {
+        String jwtSecret = System.getenv("JASSU_JWT_SECRET");
+        if(jwtSecret == null) {
             LOG.error("Env JASSU_JWT_SECRET must be set!");
             Application.exit();
         }
+        JWT_KEY = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
 
     public static String getJwt(String email) {
@@ -30,7 +34,7 @@ public class JWTAuth {
                 .setSubject(email)
                 .setClaims(claims)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .signWith(JWT_KEY)
                 .compact();
 
         LOG.debug("Generated jwt for " + email);
@@ -53,9 +57,11 @@ public class JWTAuth {
         }
 
         try {
-            return Jwts.parser()
-                    .setSigningKey(JWT_SECRET)
-                    .parseClaimsJws(token.replace(JWT_TOKEN_PREFIX,""))
+            String jwt = token.replace(JWT_TOKEN_PREFIX,"");
+            return Jwts.parserBuilder()
+                    .setSigningKey(JWT_KEY)
+                    .build()
+                    .parseClaimsJws(jwt)
                     .getBody();
         } catch (RuntimeException e) {
             return null;
